38a8ae3c90bf4b58d54b4518411d3ee18e45c5c3
Pawel Szczepanski <pxvxax@gmail.com>
2018-10-12 16:04:18 +0200

    min max dodano min

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 int main() {
     int a = 7;
     int b = 3;
-    int max;
+    int max, min;
 
     max = a > b ? a : b;
 
-    printf("maksimum: %i", max);
+    printf("maksimum: %i\n", max);
+
+    min = a < b ? a : b;
+
+    printf("minimum: %i\n", min);
 
     return 0;
 }
